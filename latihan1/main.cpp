ed347ba365e0654748272381a689e448c5fbf8b7
Muhammad Avien Kusuma <112920531+Muhammad-Avien-Kusuma@users.noreply.github.com>
2022-09-13 10:40:11 +0700

    Add files via upload

---
+++ latihan1/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world! \n" << endl;
+    cout << "Nama           : Muhammad Avien Kusuma \n";
+    cout << "Nim            : A11202214650 \n";
+    cout << "Asal sekolah   : SMAN 1 SENDAWAR \n";
+    return 0;
+}
