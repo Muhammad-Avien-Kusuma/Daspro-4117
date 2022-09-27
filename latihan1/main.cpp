8fb12777497b9972bf09d9c2eb8eb44d3249ced4
Muhammad Avien Kusuma <112920531+Muhammad-Avien-Kusuma@users.noreply.github.com>
2022-09-27 21:38:56 +0700

    Update main.cpp

--- latihan1/main.cpp
+++ latihan1/main.cpp
@@ -6,7 +6,7 @@ int main()
 {
     cout << "Hello world! \n" << endl;
     cout << "Nama           : Muhammad Avien Kusuma \n";
-    cout << "Nim            : A11202214650 \n";
+    cout << "Nim            : A11.2022.xxx \n";
     cout << "Asal sekolah   : SMAN 1 SENDAWAR \n";
     return 0;
 }
